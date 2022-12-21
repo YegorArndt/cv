ea8ce30466849648e2072c12b764c2181f05af58
Yegor Arndt <devegorarndt@gmail.com>
2022-12-21 23:01:57 +0400

    Mv up folder structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
